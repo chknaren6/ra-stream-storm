a0492cea41ca21987323ec00720532bf7bb42e2f
karthik <chknaren6@gmail.com>
2026-03-21 17:16:41 +0530

    Phase 2 & 3 verified - partitioning and scaling working

--- src/main/java/com/rastream/allocation/ResourceScaler.java
+++ src/main/java/com/rastream/allocation/ResourceScaler.java
@@ -1,6 +1,7 @@
-package com.rastream.model;
+package com.rastream.allocation;
 
 import com.rastream.dag.Task;
+import com.rastream.model.ResourceModel;
 import com.rastream.partitioning.PartitionScheme;
 import com.rastream.partitioning.Subgraph;
 
