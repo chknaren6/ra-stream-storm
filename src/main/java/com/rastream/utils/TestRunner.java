fca5a39ce2d4d88c001c9c1b3621af1132f2a3dd
karthik <chknaren6@gmail.com>
2026-03-21 17:53:40 +0530

    Phase 5 - Data rates simulation with metrics produced

--- src/main/java/com/rastream/utils/TestRunner.java
+++ src/main/java/com/rastream/utils/TestRunner.java
@@ -22,6 +22,11 @@ public class TestRunner {
         testPhase3();
         System.out.println("----------------------------");
         testPhase4();
+
+        System.out.println("----------------------------");
+        System.out.println("Phase 5: run TopologyRunner separately");
+        System.out.println("Command: mvn exec:java " +
+                "-Dexec.mainClass=\"com.rastream.topology.TopologyRunner\"");
     }
 
     public static void testPhase2() {
